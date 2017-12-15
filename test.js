543bad5eaacca6b0c00aeb7fa1d78e2138a74858
John  Tucker <john@larkintuckerllc.com>
2017-12-15 12:50:43 -0500

    lint and test

---
+++ test.js
@@ -0,0 +1,16 @@
+const { spawn } = require('child_process');
+const request = require('request');
+const test = require('tape');
+
+const child = spawn('node', ['index.js']);
+test('responds to requests', (t) => {
+  t.plan(3);
+  child.stdout.on('data', () => {
+    request('http://127.0.0.1:5000', (error, response, body) => {
+      child.kill();
+      t.false(error);
+      t.equal(response.statusCode, 200);
+      t.notEqual(body.indexOf('Hello World'), -1);
+    });
+  });
+});
