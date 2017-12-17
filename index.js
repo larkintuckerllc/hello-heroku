66f381f425b2b88f05c8f770f224c36b14e2664d
John  Tucker <john@larkintuckerllc.com>
2017-12-16 19:07:35 -0500

    First commit

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const http = require('http');
+const { Client } = require('pg');
 
 const PORT = process.env.PORT || 5000;
-
+const { DATABASE_URL } = process.env;
 const server = http.createServer((req, res) => {
+  const client = new Client({
+    connectionString: DATABASE_URL,
+  });
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  client.connect()
+    .then(() => client.query('SELECT * FROM hellotable'))
+    .then((result) => {
+      res.end(`${result.rows[0].name}\n`);
+      client.end();
+    })
+    .catch(() => {
+      res.end('ERROR');
+      client.end();
+    });
 });
-
 server.listen(PORT, () => {
   // eslint-disable-next-line
   console.log(`Server running on ${PORT}/`);
